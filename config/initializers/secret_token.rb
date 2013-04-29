dfe3e15c86edfbd4b60fa68f06bbeb0c3ae19934
apisake <apisake@gmail.com>
2013-04-29 12:51:14 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ExamVault::Application.config.secret_token = 'f3b9fefaf4d7673c0ef45b422877965faf5be4a0625072811c683e84a8e9ca52fb4620ca392565de83bce999106655c1643462875b15214afd70f021937566a9'
